d449c5fdd2e02ec698462b99d17c30e68c42741e
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-20 03:03:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,17 +50,20 @@ class Student
   end
 end
 
-def self.find_by_name(name)
-  # find the student in the database given a name	    sql = <<-SQL
+d # find the student in the database given a name	    sql = <<-SQL
   # return a new instance of the Student class	    SELECT *
-  FROM students
-  WHERE name = ?
-  LIMIT 1
-  SQL
-  DB[:conn].execute(sql,name).map do |row|
-    self.new_from_db(row)
-  end.first
-end
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM student
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
 def self.students_below_12th_grade
   sql = <<-SQL
