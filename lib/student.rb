27e01fbe6e6bc767db78d3f7ef4e2289530bbb8f
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-20 03:00:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,36 +50,43 @@ class Student
   end
 end
 
-  def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
-    sql = <<-SQL
-     SELECT *
-     FROM students
-     WHERE name = ?
-     LIMIT 1
-   SQL
+def self.find_by_name(name)
+  # find the student in the database given a name	    sql = <<-SQL
+  # return a new instance of the Student class	    SELECT * 
+  FROM students
+  WHERE name = ?
+  LIMIT 1
+  SQL
+  DB[:conn].execute(sql,name).map do |row|
+    self.new_from_db(row)
+  end.first
+end
 
-   DB[:conn].execute(sql, name).map do |row|
-     self.new_from_db(row)
-   end.first
+def self.students_below_12th_grade
+  sql = <<-SQL
+  SELECT * 
+  FROM students 
+  WHERE students.grade < 12
+  SQL
+  DB[:conn].execute(sql).collect do |row|
+    self.new_from_db(row)
   end
+end
+
+def self.first_student_in_grade_10
+  sql = <<-SQL
+  SELECT * 
+  FROM students
+  WHERE students.grade = 10
+  LIMIT 1
+  SQL
+  DB[:conn].execute(sql).collect do |row|
+    self.new_from_db(row)
+  end.first
+end	  
 
 
-  def self.first_student_in_grade_10
-    sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE students.grade = 10
-    LIMIT 1
-    SQL
-    DB[:conn].execute(sql).collect do |row|
-      self.new_from_db(row)
-    end.first
-  end
-
-
-def self.count_all_students_in_grade_9
+  def self.count_all_students_in_grade_9
   sql = <<-SQL
   SELECT *
   FROM students
@@ -88,17 +95,8 @@ def self.count_all_students_in_grade_9
   DB[:conn].execute(sql).collect do |row|
     self.new_from_db(row)
   end
+  end
 
-  def self.students_below_12th_grade
-      sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE students.grade < 12
-      SQL
-      DB[:conn].execute(sql).collect do |row|
-        self.new_from_db(row)
-      end
-    end
 
 
 
