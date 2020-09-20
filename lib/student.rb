b44efa69a301187da2a7056cef5a29ebb19367af
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-20 03:42:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,12 +65,12 @@ class Student
         WHERE name = ?
         LIMIT 1
       SQL
-   
+
       DB[:conn].execute(sql, num).map do |row|
         self.new_from_db(row)
       end
     end
-      
+
   def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT * FROM students
