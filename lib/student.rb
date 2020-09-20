3c353ccbdb425d886a6c36e3cb70fc9241c0ad86
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-20 03:30:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,13 +58,17 @@ class Student
       end
   end
 
-  def self.first_X_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
-       SELECT * FROM students WHERE grade = 10 LIMIT ?
-    SQL
+      SELECT * 
+      FROM students
+      WHERE grade = 10
+      LIMIT = ?
+      SQL
 
-    s = DB[:conn].execute(sql, x)
-    # binding.pry
+      DB[:conn].execute(sql, x).map do |row|
+        self.new_from_db(row)
+      end.first
   end
 
   def self.first_student_in_grade_10
